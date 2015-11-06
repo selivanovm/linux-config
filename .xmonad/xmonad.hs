9223869347fc722f4e9db97dbe684a5f7438d41a
Mikhail Selivanov <selivanov.mike@gmail.com>
2015-11-06 09:42:05 +0000

    merged work and home settings for XMonad

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -1,10 +1,11 @@
 import XMonad
 import XMonad.Actions.GridSelect
 import XMonad.Util.EZConfig(additionalKeys)
+import XMonad.Hooks.SetWMName
 
 main = xmonad $ defaultConfig
     { terminal    = "urxvt"
     , modMask     = mod4Mask
-    , borderWidth = 3
+    , borderWidth = 1
+    , startupHook = setWMName "LG3D"
     } `additionalKeys` [ ((mod4Mask, xK_s), spawnSelected defaultGSConfig ["emacs&", "chromium&"]) ]
-
