a4820fed1b4513edbe522e1fc79b1ac7ea39ab26
Mikhail Selivanov <selivanov.mike@gmail.com>
2015-11-06 09:56:56 +0000

    configure brightness control keys for xmonad

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -2,10 +2,13 @@ import XMonad
 import XMonad.Actions.GridSelect
 import XMonad.Util.EZConfig(additionalKeys)
 import XMonad.Hooks.SetWMName
+import Graphics.X11.ExtraTypes.XF86
 
 main = xmonad $ defaultConfig
     { terminal    = "urxvt"
     , modMask     = mod4Mask
     , borderWidth = 1
     , startupHook = setWMName "LG3D"
-    } `additionalKeys` [ ((mod4Mask, xK_s), spawnSelected defaultGSConfig ["emacs&", "chromium&"]) ]
+    } `additionalKeys` [ ((mod4Mask, xK_s)                     , spawnSelected defaultGSConfig ["emacs&", "chromium&"])
+                       , ((0, xF86XK_MonBrightnessDown)        , spawn "xbacklight -dec 20")
+                       , ((0, xF86XK_MonBrightnessUp)          , spawn "xbacklight -inc 20")]
